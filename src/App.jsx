fbd455c1cf605b6610348625f17f1f9f919da0ba
Isai Chaidez <isaichaidez@gmail.com>
2024-01-12 11:19:41 -0800

    finished the map tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Explore from './components/Explore';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 class App extends React.Component {
