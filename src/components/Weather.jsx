629836dad0c5aa59c33da3132900e73002bb83a7
Isai Chaidez <isaichaidez@gmail.com>
2024-01-16 22:20:25 -0800

    progress so far

---
+++ src/components/Weather.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// const API_KEY = import.meta.env.VITE_LOCATIONIQ_API;
+
+class Weather extends React.Component  {
+
+/*
+TODO Run get request to the local api server
+TODO Have search query look for cities that weather.json contains
+TODO
+ */
+
+    render() {
+        return(
+          <>
+          <h1>Hello</h1>
+          </>
+        )
+    }
+
+
+}
+
+export default Weather;
