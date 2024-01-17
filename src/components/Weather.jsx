0c27404b7383119a8569a60bcd4decf67ee08172
Isai Chaidez <isaichaidez@gmail.com>
2024-01-16 23:20:23 -0800

    small late night changes, prep for tomorrow

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,11 +4,6 @@ import React from 'react';
 
 class Weather extends React.Component  {
 
-/*
-TODO Run get request to the local api server
-TODO Have search query look for cities that weather.json contains
-TODO
- */
 
     render() {
         return(
